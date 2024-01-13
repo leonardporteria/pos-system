178040fa3b287ec2e9b47ebd18915266d335c8fd
leonard-porteria <porterialeonard@gmail.com>
2024-01-13 23:26:23 +0800

    cashier interface initial setup

--- client/src/components/cashier/Cashier.jsx
+++ client/src/components/cashier/Cashier.jsx
@@ -1,5 +1,22 @@
 const Cashier = () => {
-  return <div className='Cashier'>Cashier</div>;
+  return (
+    <div className='Cashier'>
+      <div className='Cashier__Header'>
+        <div className='Cashier__Header__Details'>SnapShot</div>
+        <div className='Cashier__Header__User'>User</div>
+      </div>
+      <div className='Cashier__Main'>
+        <div className='Cashier__Main__Preview'>
+          <div className='Cashier__Main__Preview__Receipt'></div>
+          <div className='Cashier__Main__Preview__Confirm'>cancel pay</div>
+        </div>
+        <div className='Cashier__Main__Action'>
+          <div className='Cashier__Main__Action__Buttons'></div>
+          <div className='Cashier__Main__Action__Numpad'></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Cashier;
